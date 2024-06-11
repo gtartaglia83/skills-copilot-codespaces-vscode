5288e25a45becbe7ab5178e2e85bd38d37275920
gtartaglia83 <giorgio.tartaglia@capgemini.com>
2024-06-11 07:56:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,32 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberValue = member.options[member.selectedIndex].value;
+  var memberText = member.options[member.selectedIndex].text;
+  var memberSkills = document.getElementById("memberSkills");
+  var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+  var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+  var memberSkillsTextValue = memberSkillsText + " (" + memberText + ")";
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/ /g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\(|\)/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/&/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/-/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\//g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\./g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/,/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/:/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/;/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/'/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/"/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/!/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\?/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/%/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/@/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/#/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\$/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\^/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\*/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\(/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\)/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/\+/g, '');
+  var memberSkillsTextValue = memberSkillsTextValue.replace(/=/g, '');
+}
